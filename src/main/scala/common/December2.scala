bfa068249f9b287ba1a983b92d59f7ff353519da
Mathieu Meylan <mathieu.meylan@nexthink.com>
2019-12-04 17:07:07 +0100

    December2 part 2

--- src/main/scala/common/December2.scala
+++ src/main/scala/common/December2.scala
@@ -21,6 +21,10 @@ object December2 {
 
     println(s"input: $input")
     println(s"res  : (${res.head} from $res")
+
+    val expected = 19690720
+    val part2res = searchForOutput(memory, expected).map { case (noun, verb) => { 100 * noun + verb } }
+    println(s"part 2, looking for $expected, found ? ${part2res}")
   }
 
   def runProgram(memory: IntCode) = {
@@ -45,5 +49,15 @@ object December2 {
   private def executeInstruction(memory: IntCode, address: Int, instruction: (Int, Int) => Int): IntCode = {
     if (memory.length < address + 4 - 1) memory
     else memory.updated(memory(address + 3), instruction(memory(memory(address + 1)), memory(memory(address + 2))))
+  }
+
+  def searchForOutput(program: IntCode, output: Int): Option[(Int, Int)] = {
+    for (noun <- 0 to 99) {
+      for (verb <- 0 to 99) {
+        val res = runProgram(program.updated(1, noun).updated(2, verb))
+        if (res.head == output) return Some(noun, verb)
+      }
+    }
+    None
   }
 }
