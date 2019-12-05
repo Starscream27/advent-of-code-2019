33d87629fb54d3ac3c02aca9becc3514e1d65967
Mathieu Meylan <mathieu.meylan@nexthink.com>
2019-12-05 12:14:26 +0100

    December 4 part 2

--- src/main/scala/common/December4.scala
+++ src/main/scala/common/December4.scala
@@ -1,5 +1,7 @@
 package common
 
+import scala.collection.mutable.ListBuffer
+
 object December4 {
   def main(args: Array[String]): Unit = {
     val matches = (236491 to 713787)
@@ -7,6 +9,12 @@ object December4 {
       .count(_ == true)
 
     println(s"Number of matches found: $matches")
+
+    val matchesPart2 = (236491 to 713787)
+      .map(matchesPasswordRules2)
+      .count(_ == true)
+
+    println(s"Number of refined (part2) matches found: $matchesPart2")
   }
 
   private def twoAdjacentDigitsAreTheSame(password: Int): Boolean = {
@@ -30,6 +38,42 @@ object December4 {
     true
   }
 
+  private def atLeastOneGroupOfTwoEqualDigitsAreIsolated(password: Int): Boolean = {
+    def findAllGroupOfTwoEqualDigitsIndexes(digits: Array[Char]): List[(Int, Int)] = {
+      val adjacentValuesIndexes = ListBuffer[(Int, Int)]()
+      for ((currentValue, index) <- digits.view.zipWithIndex) {
+        digits.lift(index - 1) match {
+          case Some(prevValue) if currentValue == prevValue => {
+            adjacentValuesIndexes.addOne((index - 1, index))
+          }
+          case _ => {}
+        }
+      }
+      adjacentValuesIndexes.toList
+    }
+    def isDigitAtIndexEqualThan(digits: Array[Char], index: Int, than: Char) = {
+        digits.lift(index) match {
+          case Some(value) if value == than => true
+          case _ => false
+        }
+    }
+
+    val digits = password.toString.toArray
+    val adjacentValues = findAllGroupOfTwoEqualDigitsIndexes(digits)
+
+    val numberOfIsolatedAdjacentPairs = adjacentValues.map(indexes => {
+      val value = digits(indexes._1)
+      val previousIsSame = isDigitAtIndexEqualThan(digits, indexes._1 - 1, value)
+      val nextIsSame = isDigitAtIndexEqualThan(digits, indexes._2 + 1, value)
+      previousIsSame || nextIsSame
+    }).count(_ == false)
+
+    numberOfIsolatedAdjacentPairs > 0
+  }
+
   def matchesPasswordRules(password: Int): Boolean =
     twoAdjacentDigitsAreTheSame(password) && digitsAlwaysIncrease(password)
+
+  def matchesPasswordRules2(password: Int): Boolean =
+    twoAdjacentDigitsAreTheSame(password) && atLeastOneGroupOfTwoEqualDigitsAreIsolated(password) && digitsAlwaysIncrease(password)
 }
--- src/test/scala/common/December4Spec.scala
+++ src/test/scala/common/December4Spec.scala
@@ -8,4 +8,9 @@ class December4Spec extends FlatSpec with Matchers {
     December4.matchesPasswordRules(223450) shouldBe false
     December4.matchesPasswordRules(123789) shouldBe false
   }
+  "December4" should "correctly assess if a number match the password rules with added criterias" in {
+    December4.matchesPasswordRules2(112233) shouldBe true
+    December4.matchesPasswordRules2(123444) shouldBe false
+    December4.matchesPasswordRules2(111122) shouldBe true
+  }
 }
